b62a27c4b3a60784c2cfc4bf3c2f803b520c4ce9
Adby Santos <adby.santos15@outlook.com>
2022-08-09 23:57:22 -0300

    feat: local notifications

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:battery_indicator/battery_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'home_store.dart';
 
@@ -13,6 +14,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final store = Modular.get<HomeStore>();
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  late AndroidInitializationSettings androidInitializationSettings;
+  late InitializationSettings initializationSettings;
 
   var buildCounter = 0;
 
@@ -22,7 +26,37 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    initializeNotifications();
     startListeningToTheBatteryValue();
+  }
+
+  void initializeNotifications() {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    androidInitializationSettings = const AndroidInitializationSettings('app_icon');
+    initializationSettings = InitializationSettings(
+      android: androidInitializationSettings,
+    );
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> showNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'your channel id',
+      'your channel name',
+      channelDescription: 'your channel description',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+      fullScreenIntent: true,
+    );
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Seu celular agradece:',
+      'Preserve a bateria dele tirando-o da tomada',
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
   }
 
   void startListeningToTheBatteryValue() {
@@ -33,6 +67,9 @@ class _HomePageState extends State<HomePage> {
   void onNewBatteryValueCallback(dynamic possibleValue) {
     final newBatteryValue = possibleValue as double?;
     if (newBatteryValue != null && newBatteryValue != currentBatteryValue) {
+      if (newBatteryValue == 80 && previousBatteryValue < 80) {
+        showNotification();
+      }
       setState(() {
         previousBatteryValue = currentBatteryValue;
         currentBatteryValue = newBatteryValue;
@@ -62,6 +99,11 @@ class _HomePageState extends State<HomePage> {
                       style: TextStyle(fontSize: 40),
                     ),
                     BatteryIndicator(batteryFromPhone: false, batteryLevel: currentBatteryValue.toInt(), size: 36, percentNumSize: 27),
+                    TextButton(
+                        onPressed: () {
+                          showNotification();
+                        },
+                        child: const Text('Show notification'))
                   ],
                 ),
               ],
