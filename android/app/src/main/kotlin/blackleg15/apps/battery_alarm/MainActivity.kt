9c1112b8257ebd719c854af3f517700b3cf28c95
Adby Santos <adby.santos15@outlook.com>
2022-08-31 10:46:19 -0300

    feat: functions to start/stop service on Android

--- android/app/src/main/kotlin/blackleg15/apps/battery_alarm/MainActivity.kt
+++ android/app/src/main/kotlin/blackleg15/apps/battery_alarm/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class NotificationServiceResultReceiver(val callback: (Float) -> Unit, handler: Handler?) : ResultReceiver(handler) {
+class NotificationServiceResultReceiver(val callback: (Float) -> Unit, handler: Handler?) :
+    ResultReceiver(handler) {
     override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
         super.onReceiveResult(resultCode, resultData)
         val data: Float? = resultData?.getFloat("battery")
@@ -19,30 +21,64 @@ class NotificationServiceResultReceiver(val callback: (Float) -> Unit, handler: 
 
 class MainActivity : FlutterActivity() {
     private val channelStream = "adbysantos.apps.battery_alarm/stream"
+    private val methodChannel = "adbysantos.apps.battery_alarm/call"
+    private lateinit var onNewBatteryValueCallback: (Float) -> Unit
 
-    fun initService(callback: ((Float) -> Unit)) {
+    private fun initService(callback: (Float) -> Unit) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val serviceIntent = Intent(this, BatteryNotificationService::class.java)
-            serviceIntent.putExtra("notification_callback", NotificationServiceResultReceiver(callback, null))
+            serviceIntent.putExtra(
+                "notification_callback",
+                NotificationServiceResultReceiver(callback, null)
+            )
             startService(serviceIntent)
         }
+    }
+
+    private fun stopService() {
+        stopService(Intent(this, BatteryNotificationService::class.java))
     }
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            methodChannel
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "initService" -> {
+                    try {
+                        initService(onNewBatteryValueCallback)
+                        result.success(null)
+                    } catch (e: Error) {
+                        result.error("START", "Couldn't start the battery service", e.message)
+                    }
+                }
+                "stopService" -> {
+                    try {
+                        stopService()
+                        result.success(null)
+                    } catch (e: Error) {
+                        result.error("STOP", "Couldn't stop the battery service", e.message)
+                    }
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
 
         EventChannel(flutterEngine.dartExecutor.binaryMessenger, channelStream)
             .setStreamHandler(
                 object : EventChannel.StreamHandler {
                     override fun onListen(args: Any?, events: EventChannel.EventSink) {
-                        val onNewBatteryValueCallback = fun(value: Float) { events.success(value) }
-                        initService(onNewBatteryValueCallback)
-                        println("init service")
+                        onNewBatteryValueCallback = fun(value: Float) { events.success(value) }
+                        println("registering on new battery value callback")
                     }
 
                     override fun onCancel(arguments: Any?) {
-                        println("cancelling listener")
+                        println("cancelling event channel")
                     }
 
                 }
